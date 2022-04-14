7540daba65eacee0ab840aaa6a5f5b36b702714b
Arnaud-Ferreira <ferreira.arnaud@hotmail.fr>
2022-04-14 19:08:55 +0200

    static display

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,5 +1,28 @@
 <template>
-    <h1>Hello Weather</h1>
+    <div class="container">
+
+        <h1 class="my-4">Weather application with Vue Js</h1>
+
+        <div class="form-groupe mb-5">
+            <label for="position">Enter a city name</label>
+            <input
+            id="position"
+            type="text"
+            class="form-control mt-3"
+            v-model="request"
+            v-on:keypress="goWeather"
+            >
+        </div>
+
+        <div class="w75 mb-4">
+            <h3 class="text-center">Position : Paris</h3>
+        </div>
+
+        <div class="card text-center p-5">
+            <p class="showed-text">Temperature : 20°</p>
+            <p class="showed-text">Weather conditions : Cloudy</p>
+        </div>
+    </div>
 </template>
 
 
